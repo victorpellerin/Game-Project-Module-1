cb5f21ad0dfbf05400d3d41c5b515ec46acf97ef
Victor Pellerin <pellerin.victor@gmail.com>
2018-01-22 15:37:55 +0100

    add images and image catalog

---
+++ javascript/catalogs.js
@@ -0,0 +1,13 @@
+'use strict'
+
+
+var catalog = [
+  './img/img1.jpeg',
+  './img/img2.jpeg',
+  './img/img3.jpeg',
+  './img/img4.jpeg',
+  './img/img5.jpeg',
+  './img/img6.jpeg',
+  './img/img7.jpeg',
+  './img/img8.jpeg',
+];
